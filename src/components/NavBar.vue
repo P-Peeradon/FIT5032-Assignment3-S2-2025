2b8d1361301e174c6ae77c27b93b843f641b01d5
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-10-05 20:23:24 +1100

    Adding toggle for dropdown menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,14 +10,12 @@
                     <li class="nav-item dropdown">
                         <a
                             href="#"
+                            @click="toggleConnect"
                             role="button"
                             class="nav-link dropdown-toggle"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
+                            >Connect</a
                         >
-                            Connect
-                        </a>
-                        <ul class="dropdown-menu">
+                        <ul v-show="connectPillar" class="dropdown-menu">
                             <li>
                                 <router-link class="drowdown-item" to="/connect/society">
                                     Community
@@ -26,10 +24,36 @@
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a href="#" role="button" class="nav-link">Reflect</a>
+                        <a
+                            href="#"
+                            @click="toggleReflect"
+                            role="button"
+                            class="nav-link dropdown-toggle"
+                            >Reflect</a
+                        >
+                        <ul v-show="reflectPillar" class="dropdown-menu">
+                            <li>
+                                <router-link class="dropdown-item" to="/reflect/journal">
+                                    Journal
+                                </router-link>
+                            </li>
+                        </ul>
                     </li>
                     <li class="nav-item">
-                        <a href="#" role="button" class="nav-link">Grow</a>
+                        <a
+                            href="#"
+                            @click="toggleGrow"
+                            role="button"
+                            class="nav-link dropdown-toggle"
+                            >Grow</a
+                        >
+                        <ul v-show="growPillar" class="dropdown-menu">
+                            <li>
+                                <router-link class="drowdown-item" to="/grow/education">
+                                    Mental Education
+                                </router-link>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
             </div>
@@ -38,12 +62,33 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
 
-const navShow = ref(false)
+const navShow = ref(false);
+const connectPillar = ref(false);
+const reflectPillar = ref(false);
+const growPillar = ref(false);
+
 const toggleNav = () => {
-    navShow.value = !navShow.value
-}
+    navShow.value = !navShow.value;
+    if (!navShow.value) {
+        connectPillar.value = false;
+        reflectPillar.value = false;
+        growPillar.value = false;
+    }
+};
+
+const toggleConnect = () => {
+    connectPillar.value = !connectPillar.value;
+};
+
+const toggleReflect = () => {
+    reflectPillar.value = !reflectPillar.value;
+};
+
+const toggleGrow = () => {
+    growPillar.value = !growPillar.value;
+};
 </script>
 
 <style></style>
