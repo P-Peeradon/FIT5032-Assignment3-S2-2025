3241b51a8da336cd0a2875be7c3974f12fe017a5
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-10-05 19:34:27 +1100

    Defining three pillars: connect, reflect, grow

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,8 +14,24 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navigation">
-                <ul>
-                    <li></li>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a
+                            href="#"
+                            role="button"
+                            class="nav-link dropdown-toggle"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                        >
+                            Connect
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" role="button" class="nav-link">Reflect</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" role="button" class="nav-link">Grow</a>
+                    </li>
                 </ul>
             </div>
         </div>
