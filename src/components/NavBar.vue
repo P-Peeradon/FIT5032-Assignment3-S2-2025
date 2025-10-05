4dd2e78ccc8eab2431b9f94a70a941614aa0551b
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-10-05 19:22:05 +1100

    Install Bootstrap

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,25 @@
 <template>
-    <nav></nav>
+    <nav class="navbar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Chillax Corner</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navigation"
+                aria-controls="navigation"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navigation">
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script setup></script>
---
+++ src/components/WebHeader.vue
@@ -0,0 +1,9 @@
+<template>
+    <NavBar />
+</template>
+
+<script setup>
+import NavBar from './NavBar.vue'
+</script>
+
+<style></style>
