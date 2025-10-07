8e9c2ac78a4be6bc146d4666657d225feb9abda6
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-10-07 18:14:07 +1100

    Update Address

--- src/components/WebFooter.vue
+++ src/components/WebFooter.vue
@@ -8,7 +8,26 @@
                     class="image-fluid mx-auto my-auto"
                 />
             </div>
-            <div class="col-8"></div>
+            <div class="col-8 d-flex flex-column">
+                <h3>Contact</h3>
+                <ul class="list-group">
+                    <li class="list-group-item">Tel: +61 48 024 1025 (Australia)</li>
+                    <li class="list-group-item">Tel: +64 48 817 0252 (New Zealand)</li>
+                    <li class="list-group-item">Tel: +65 48 023 1855 (Singapore)</li>
+                </ul>
+                <h3>Admin Address</h3>
+                <ul class="list-group">
+                    <li class="list-group-item">Melbourne: 12/45 Flinder St Southbank VIC 3006</li>
+                    <li class="list-group-item">Sydney: 3A/20 Macquarie St Sydney NSW 2000</li>
+                    <li class="list-group-item">Adelaide: 100 Park Terrace Adelaide SA 5000</li>
+                    <li class="list-group-item">
+                        Auckland: FLAT 2/12 Brightside Rd Mount Eden Auckland 1024 NEW ZEALAND
+                    </li>
+                    <li class="list-group-item">
+                        Singapore: BLK 102 Jurong E St 13#12-05 Jurong Gty Singapore 600102
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
