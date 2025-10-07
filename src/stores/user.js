fec7b14b2271d6bdbe1d3321521361261265863f
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-10-08 10:58:23 +1100

    Handling stores and entities

--- src/stores/user.js
+++ src/stores/user.js
@@ -29,6 +29,45 @@ const authStore = defineStore('auth', () => {
     };
 });
 
-const userStore = defineStore('user', () => {});
+const userStore = defineStore('user', () => {
+    const uid = authStore.userID;
+    const email = ref('');
+    const role = ref('');
+    const username = ref('');
+    const journals = ref([]);
+    const communities = ref([]);
+
+    const fetchUserData = async () => {
+        try {
+            const userState = await axios.get('', { userId: uid });
+
+            email.value = userState.email;
+            role.value = userState.role;
+            username.value = userState.username;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const fetchUserJournal = async () => {
+        try {
+            const userJournal = await axios.get('', { userId: uid, role: role.value }); // API that get user journal
+
+            journals.value = userJournal;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const fetchUserCommunity = async () => {
+        try {
+            const userCommunity = await axios.get('', { userId: uid, role: role.value }); // API that get user community
+
+            communities.value = userCommunity;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+});
 
 export { authStore, userStore };
