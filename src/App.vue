a12038ecc5808a8e4955fb519683c81dd3dd95c7
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-10-14 13:55:26 +1100

    Define style for h1, h2

--- src/App.vue
+++ src/App.vue
@@ -21,17 +21,12 @@ onAuthStateChanged(auth, () => {
     <footer><WebFooter /></footer>
 </template>
 
-<style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+<style>
+h1 {
+    color: blueviolet;
 }
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
+
+h2 {
+    color: greenyellow;
 }
 </style>
