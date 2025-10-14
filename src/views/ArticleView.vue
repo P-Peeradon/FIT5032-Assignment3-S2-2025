76b4da6aaa71b06a6b1ac3a10d9b09a613d7b09d
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-10-14 13:50:27 +1100

    Add view to display article

---
+++ src/views/ArticleView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container-fluid">
+        <h1>{{ article.title }}</h1>
+    </div>
+</template>
+
+<script setup>
+import { Article } from '../assets/article';
+import { ref, computed, onMounted } from 'vue';
+
+import { authStore } from '../stores/user';
+
+const authState = authStore();
+const fetchedArticles = ref([]);
+
+const sortByTitle = () => {
+    fetchedArticles.value.sort((a, b) => a.title.localeCompare(b.title));
+};
+
+watch();
+
+onMounted(() => {
+    authState.initAuth();
+});
+</script>
+
+<style></style>
