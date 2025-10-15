299663c4034ae9e1204aeb80677f519890fc74ea
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-10-15 21:16:41 +1100

    Import meta on vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-const FIREBASE_API_KEY = process.env.VITE_FIREBASE_API_KEY;
-const FIREBASE_AUTH_DOMAIN = process.env.VITE_FIREBASE_AUTH_DOMAIN;
-const FIREBASE_APP_ID = process.env.VITE_FIREBASE_APP_ID;
-const FIREBASE_MEASUREMENT_ID = process.env.VITE_FIREBASE_MEASUREMENT_ID;
+const FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
+const FIREBASE_AUTH_DOMAIN = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+const FIREBASE_APP_ID = import.meta.env.VITE_FIREBASE_APP_ID;
+const FIREBASE_MEASUREMENT_ID = import.meta.env.VITE_FIREBASE_MEASUREMENT_ID;
 
 // https://vite.dev/config/
 export default defineConfig({
