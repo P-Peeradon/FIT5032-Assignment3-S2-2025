d91559525c37ffbf4a26996be94345bb87262e4a
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-10-16 22:06:58 +1100

    Remove module import error

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -5,7 +5,7 @@
 <script setup>
 import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { onBeforeUnmount, onMounted, ref } from 'vue';
 
@@ -52,13 +52,7 @@ onMounted(() => {
             });
         }
         map.addControl(new mapboxgl.NavigationControl()); // Navigation
-        map.addControl(
-            new MapboxDirections({
-                accessToken: mapboxgl.accessToken,
-                unit: 'metric',
-                profile: 'mapbox/driving',
-            })
-        );
+
         map.addControl(
             new mapboxgl.GeolocateControl({
                 positionOptions: { enableHighAccuracy: true },
