87af46915885538e09041b90a79f84bbb74767eb
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-10-16 17:24:51 +1100

    Fix Profile Bar Logic

--- src/components/ProfileBar.vue
+++ src/components/ProfileBar.vue
@@ -1,12 +1,12 @@
 <template>
     <nav class="container-fluid bg-info d-flex flex-row pt-2 pb-2 mt-lg-0 justify-content-end">
-        <div v-if="!authState.isAuthenticated" class="btn-group me-5" role="group">
-            <router-link class="btn btn-light" to="/register"> Register </router-link>
-            <router-link class="btn btn-primary" to="/login">Login</router-link>
-        </div>
-        <div v-else class="btn-group">
+        <div v-if="authState.isAuthenticated" class="btn-group">
             <p class="me-3">{{ userState.username }}</p>
             <button class="btn btn-danger" @click="logout">Logout</button>
+        </div>
+        <div v-else class="btn-group me-5" role="group">
+            <router-link class="btn btn-light" to="/register"> Register </router-link>
+            <router-link class="btn btn-primary" to="/login">Login</router-link>
         </div>
     </nav>
 </template>
